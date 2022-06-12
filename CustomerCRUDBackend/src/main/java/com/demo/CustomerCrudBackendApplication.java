1182ace87cfafcafcb5b8d996545c39cad0192c7
Aniket.Bonde <aniket.bonde@communitybrands.com>
2022-06-12 21:28:14 +0530

    added sysout for triggering pipeling in codebuild aws

--- CustomerCRUDBackend/src/main/java/com/demo/CustomerCrudBackendApplication.java
+++ CustomerCRUDBackend/src/main/java/com/demo/CustomerCrudBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerCrudBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Let's gooooooooooo........");
 		SpringApplication.run(CustomerCrudBackendApplication.class, args);
 	}
 
